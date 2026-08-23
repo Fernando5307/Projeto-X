fb565a5301d4d3763713845274ba38d0f7e42af1
koharu <luizfelipepereirachagas2019@gmail.com>
2026-08-22 22:44:49 -0300

    tudo certinho

--- script.js
+++ script.js
@@ -2,14 +2,18 @@
 let nome = document.getElementById("nome");
 let idade = document.getElementById("idade");
 let botao = document.getElementById("inscrever");
+let telefone = document.getElementById("telefone");
+let email = document.getElementById ("email");
 // ERRO: elemento possui outro id no HTML
 let mensagem = document.getElementById("mensagem");
 // Evento do botão
 botao.addEventListener("click", function () {
  let nomeDigitado = nome.value;
  let idadeDigitada = idade.value;
+ let emailDigitado = email.value;
+  let telefoneDigitado = telefone.value; 
  // ERRO: condição incompleta
- if (nomeDigitado == "" && idadeDigitada == "") {
+ if (nomeDigitado == "" || idadeDigitada == "" || emailDigitado =="" || telefoneDigitado =="") {
  mensagem.textContent =
  "Preencha todos os campos antes de continuar.";
  }
